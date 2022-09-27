ddaa43fa4e8ff6558d434bee5a2d7e12af752762
Oliver Schantz <frequency403@gmail.com>
2022-09-27 16:15:20 +0200

    Made MIT a concat string

--- src/licences.rs
+++ src/licences.rs
@@ -29,9 +29,9 @@ Please make sure to update tests as appropriate.
 }
 
 pub fn mit() -> String {
-    "MIT License
+    ["MIT License
 
-Copyright (c) 2022 Oliver Schantz
+Copyright (c) ", "2022 Oliver Schantz" , "
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the \"Software\"), to deal
@@ -49,5 +49,5 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.".to_string()
+SOFTWARE."].concat()
 }
